9bdd036f0e08826a2445f5976fb21ae2af1c8030
André Baldeweg <kontakt@andrebaldeweg.de>
2019-03-07 14:40:13 +0100

    users

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This script is part of baldeweg/incwadi-core
+ *
+ * Copyright 2019 André Baldeweg <kontakt@andrebaldeweg.de>
+ */
+
+namespace Baldeweg\Repository;
+
+use Baldeweg\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+}
