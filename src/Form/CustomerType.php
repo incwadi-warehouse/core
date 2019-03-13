bee75ad33984aa983b2d88282c042e5f6f3b5964
André Baldeweg <kontakt@andrebaldeweg.de>
2019-03-13 22:18:07 +0100

    added form

---
+++ src/Form/CustomerType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Baldeweg\Form;
+
+use Baldeweg\Entity\Customer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('notes')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Customer::class,
+        ]);
+    }
+}
