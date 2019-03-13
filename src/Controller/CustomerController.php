54dc94cfe63d1e5775fcd7cb8b1b75edfc8f80e7
André Baldeweg <kontakt@andrebaldeweg.de>
2019-03-13 22:18:43 +0100

    added customer controller

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * This script is part of baldeweg/incwadi-core
+ *
+ * Copyright 2019 André Baldeweg <kontakt@andrebaldeweg.de>
+ * MIT-licensed
+ */
+
+namespace Baldeweg\Controller;
+
+use Baldeweg\Entity\Book;
+use Baldeweg\Entity\Genre;
+use Baldeweg\Entity\Customer;
+use Baldeweg\Form\GenreType;
+use Baldeweg\Form\CustomerType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/v1/customer", name="customer_")
+ */
+class CustomerController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"}, name="index")
+     * @Security("is_granted('ROLE_USER')")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json(
+            $this->getDoctrine()->getRepository(Customer::class)->findAll()
+        );
+    }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, name="show")
+     * @Security("is_granted('ROLE_USER')")
+     */
+    public function show(Request $request, Customer $customer): JsonResponse
+    {
+        return $this->json($customer);
+    }
+
+    /**
+     * @Route("/new", methods={"POST"}, name="new")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function new(Request $request): JsonResponse
+    {
+        $customer = new Customer();
+        $form = $this->createForm(CustomerType::class, $customer);
+
+        $form->submit(
+            json_decode(
+                $request->getContent(),
+                true
+            )
+        );
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($customer);
+            $em->flush();
+
+            return $this->json($customer);
+        }
+
+        return $this->json([
+            'msg' => 'Please enter a valid customer!'
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", methods={"PUT"}, name="edit")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function edit(Request $request, Customer $customer): JsonResponse
+    {
+        $form = $this->createForm(CustomerType::class, $customer);
+
+        $form->submit(
+            json_decode(
+                $request->getContent(),
+                true
+            )
+        );
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->json($customer);
+        }
+
+        return $this->json([
+            'msg' => 'Please enter a valid customer!'
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", methods={"DELETE"}, name="delete")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function delete(Customer $customer): JsonResponse
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($customer);
+        $em->flush();
+
+        return $this->json([
+            'msg' => 'The customer was successfully deleted.'
+        ]);
+    }
+}
