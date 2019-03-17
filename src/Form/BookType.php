c6501865a7a7a63bcbb6409a1aaaaf369e52f09c
André Baldeweg <kontakt@andrebaldeweg.de>
2019-03-17 19:17:05 +0100

    transform the date

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -13,12 +13,16 @@ use Baldeweg\Entity\Book;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class BookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('added', TextType::class)
             ->add('author')
             ->add('genre')
             ->add('price')
@@ -27,6 +31,17 @@ class BookType extends AbstractType
             ->add('yearOfPublication')
             ->add('type')
             ->add('premium')
+        ;
+
+        $builder->get('added')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($date) {
+                    return (string)$date->getTimestamp();
+                },
+                function ($date) {
+                    return new \DateTime('@' . $date);
+                }
+            ))
         ;
     }
 
