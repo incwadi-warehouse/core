c2089dea8afef1d9519d91659ed1d24ebbbdfaf5
André Baldeweg <kontakt@andrebaldeweg.de>
2019-03-20 18:04:34 +0100

    added new branch comand

---
+++ src/Command/NewBranchCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This script is part of baldeweg/incwadi-core
+ *
+ * Copyright 2019 André Baldeweg <kontakt@andrebaldeweg.de>
+ * MIT-licensed
+ */
+
+namespace Baldeweg\Command;
+
+use Baldeweg\Entity\Branch;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class NewBranchCommand extends Command
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('branch:new')
+            ->setDescription('Creates a new branch.')
+            ->setHelp('This command creates a new branch.')
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the branch')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $name = $input->getArgument('name');
+
+        $branch = new Branch();
+        $branch->setName($name);
+
+        $this->em->persist($branch);
+        $this->em->flush();
+
+        $io->success('Branch "' . $branch->getName() . '" successfully created!');
+
+        return null;
+    }
+}
