5611981f8e0986e64d8f0fbaeaea3f8918edf7ce
André Baldeweg <kontakt@andrebaldeweg.de>
2019-08-15 19:14:35 +0200

    csfixer

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -9,8 +9,6 @@
 
 namespace Incwadi\Core\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
