cfdab5e6f6cae7bacf0f69a4adcff348b6cb8d49
André Baldeweg <kontakt@andrebaldeweg.de>
2019-11-26 16:08:09 +0100

    build: upgrade

--- config/bundles.php
+++ config/bundles.php
@@ -1,14 +1,9 @@
 <?php
 
-/*
- * This script is part of incwadi/core
- */
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
