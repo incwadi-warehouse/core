062ab26c8b1f705077eaf90b29d4e227bf2a9df2
André Baldeweg <kontakt@andrebaldeweg.de>
2020-08-17 21:13:41 +0200

    build: csfixer

--- src/Migrations/Version20200612125600.php
+++ src/Migrations/Version20200612125600.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+/*
+ * This script is part of incwadi/core
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,24 +16,24 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20200612125600 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE report (id INT AUTO_INCREMENT NOT NULL, branch_id INT NOT NULL, name VARCHAR(255) NOT NULL, search_term VARCHAR(255) DEFAULT NULL, limit_to INT NOT NULL, sold TINYINT(1) NOT NULL, removed TINYINT(1) NOT NULL, older_then_x_months INT DEFAULT NULL, branches VARCHAR(255) DEFAULT NULL, genres VARCHAR(255) DEFAULT NULL, lend_more_then_x_months INT DEFAULT NULL, order_by VARCHAR(255) DEFAULT NULL, release_year INT DEFAULT NULL, type VARCHAR(255) DEFAULT NULL, INDEX IDX_C42F7784DCD6CC49 (branch_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE report ADD CONSTRAINT FK_C42F7784DCD6CC49 FOREIGN KEY (branch_id) REFERENCES branch (id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE report');
     }
