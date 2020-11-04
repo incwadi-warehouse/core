8edc87cbf3b643224a4aee42b24891d47dfd4f4d
André Baldeweg <kontakt@andrebaldeweg.de>
2020-11-04 14:24:47 +0100

    csfixer

--- config/preload.php
+++ config/preload.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * This script is part of incwadi/core
+ */
+
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
