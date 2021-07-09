2594bc2c6b98c86eed3ba315714fd16ac9acfe6f
André Baldeweg <kontakt@andrebaldeweg.de>
2021-07-09 11:27:38 +0200

    update dependendencies

--- config/preload.php
+++ config/preload.php
@@ -1,9 +1,5 @@
 <?php
 
-/*
- * This script is part of incwadi/core
- */
-
-if (file_exists(dirname(__DIR__).'/var/cache/prod/Incwadi_Core_KernelProdContainer.preload.php')) {
-    require dirname(__DIR__).'/var/cache/prod/Incwadi_Core_KernelProdContainer.preload.php';
+if (file_exists(dirname(__DIR__). '/var/cache/prod/Incwadi_Core_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__). '/var/cache/prod/Incwadi_Core_KernelProdContainer.preload.php';
 }
