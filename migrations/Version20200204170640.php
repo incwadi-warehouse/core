d151425de00dd73c4ccfb551cad9f824187b59d6
André <a@baldeweg.org>
2023-04-15 15:02:34 +0000

    Fix issue when import fixtures

--- migrations/Version20200204170640.php
+++ migrations/Version20200204170640.php
@@ -23,7 +23,6 @@ final class Version20200204170640 extends AbstractMigration
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE refresh_tokens (id INT AUTO_INCREMENT NOT NULL, refresh_token VARCHAR(128) NOT NULL, username VARCHAR(255) NOT NULL, valid DATETIME NOT NULL, UNIQUE INDEX UNIQ_9BACE7E1C74F2195 (refresh_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE staff RENAME INDEX idx_81398e09dcd6cc49 TO IDX_426EF392DCD6CC49');
     }
 
     public function down(Schema $schema): void
@@ -32,6 +31,5 @@ final class Version20200204170640 extends AbstractMigration
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE refresh_tokens');
-        $this->addSql('ALTER TABLE staff RENAME INDEX idx_426ef392dcd6cc49 TO IDX_81398E09DCD6CC49');
     }
 }
